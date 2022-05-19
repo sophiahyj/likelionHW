39497d0d81697bfdc463fdbfc8c1840e82dbb697
sophiahyj <sophiahyj@korea.ac.kr>
2022-05-19 17:28:32 +0900

    10th hw submit

---
+++ Session10_HW/Postproject/Postapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        new_post = Post.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content']
+        )
+
+        return redirect ('detail_page', new_post.pk)
+    return render(request, 'create_post.html')
+
+def detail_page(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.method == 'POST':
+        content = request.POST['content']
+        Comment.objects.create(
+            post=post,
+            content=content
+        )
+        return redirect('detail_page', post_pk)
+
+    return render(request, 'detail_page.html', {"post": post})
+
+def edit_post(request, post_pk):
+    post = Post.objects.filter(pk=post_pk)
+    if request.method == 'POST':
+        post.update(
+            title = request.POST['title'],
+            content = request.POST['content']
+        )
+        return redirect ('detail_page', post_pk)
+    return render(request, 'edit_post.html', {'post': post[0]})
+
+
+def delete_post(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+    return redirect('home')
+    pass
+
+def delete_comment(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('detail_page', post_pk)
